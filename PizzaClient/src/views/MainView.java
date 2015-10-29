690d99aa8ea84f2c2606fa84e34fd759cb38ed94
Nick Levenduski <nicklev@rams.colostate.edu>
2015-10-29 01:51:06 -0600

    Login and create account finally complete! Creating a new account always creates a new account one AuthLevel lower than the account that created it (with the exception of the bottom level)
    UserName is now displayed on MainView
    
    NOTE***	Now the changeMenu and chefView buttons are HIDDEN by default that means you must log in to an account with a higher authlevel to see them.

--- PizzaClient/src/views/MainView.java
+++ PizzaClient/src/views/MainView.java
@@ -22,11 +22,14 @@ public class MainView extends javax.swing.JPanel {
     public MainView() {
         initComponents();
         vc = new ViewController(menuListModel);
+        customInit();
     }
 
     public MainView(ViewController vc)
     {
         this.vc = vc;
+        if(vc.getMenu().size() != menuListModel.size())
+            vc = new ViewController(menuListModel);
         initComponents();
         customInit();
     }
@@ -130,8 +133,6 @@ public class MainView extends javax.swing.JPanel {
                 updateMenuActionPerformed(evt);
             }
         });
-        if(vc.getUser().getAuthLevel()>2)
-        updateMenu.setVisible(false);
 
         chefView.setText("Chef View");
         chefView.addActionListener(new java.awt.event.ActionListener() {
@@ -139,8 +140,6 @@ public class MainView extends javax.swing.JPanel {
                 chefViewActionPerformed(evt);
             }
         });
-        if(vc.getUser().getAuthLevel()>3)
-        chefView.setVisible(false);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
