5fc9de87a816bb9d341d7850ea96f542d5c62540
Nick Levenduski <nicklev@rams.colostate.edu>
2015-10-29 05:24:12 -0600

    Merge origin/master
    Conflicts:
    	PizzaClient/src/views/MainView.form

---
+++ PizzaCommonTests/test/models/MenuModelTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import javax.swing.DefaultListModel;
+import org.junit.Test;
+
+/**
+ *
+ * @author Nick
+ */
+public class MenuModelTest {
+
+    @Test
+    public void TestMenuModelInit(){
+        //test init
+    } 
+    
+    @Test
+    public void TestMenuModelUpdate(){
+        //test updateCombined
+    }
+    
+    @Test
+    public void TestMenuModelNewItem(){
+        //test addItem
+    }
+    
+    @Test
+    public void TestMenuModelRemove(){
+        //test remove
+    }
+    
+    @Test
+    public void TestMenuModelgetItems(){
+        //test getItems
+    }
+    
+    @Test
+    public void TestMenuModelNewSpecialPrice(){
+        //test NewSpecialPrice
+    }
+    
+    @Test
+    public void TestMenuModelRevokeSpecial(){
+        //test RevokeSpecialModel
+    }
+
+    @Test
+    public void TestMenuModelSave(){
+        //test Save
+    }
+    
+    @Test
+    public void TestMenuModelgetById(){
+        //test getById
+    }
+    
+    @Test
+    public void TestMenuModelLoad(){
+        //test load
+    }
+    
+    @Test
+    public void TestMenuModelGet(){
+        //test get
+    }
+}
---
+++ PizzaCommonTests/test/testObjects/TestItemModel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testObjects;
+
+import data.IDataSource;
+import exceptions.LoadException;
+import java.util.HashMap;
+import models.IModel;
+
+/**
+ *
+ * @author Nick
+ */
+public class TestItemModel implements IModel {
+
+    @Override
+    public void save() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void get() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void init(IDataSource source) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void load(HashMap<String, Object> fields) throws LoadException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
